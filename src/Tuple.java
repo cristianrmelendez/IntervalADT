0f5a838cc52f84bf03c3908ae1bcf4c0e19e5e72
cristianrmelendez <cristian.melendez@upr.edu>
2018-03-20 13:09:21 -0400

    Fix intersection

--- src/Tuple.java
+++ src/Tuple.java
@@ -1,11 +1,11 @@
 
 public class Tuple implements Comparable<Tuple> {
-	
+
 	private Double min; 
 	private Double max;
 	//private boolean includedMin
 	//private boolean includedMax
-	
+
 	public Tuple(Double min, Double max) { 
 		if (min >= max)
 			throw new IllegalArgumentException("Min cannot be greater or equal to Max");
@@ -28,32 +28,60 @@ public class Tuple implements Comparable<Tuple> {
 	public void setMax(Double max) {
 		this.max = max;
 	}
-	
+
 	/**
 	 * Verifies if two tuples intersect and if they do returns a new tuple with the intersection of them.
 	 * @param t1 first tuple to compare
 	 * @return Returns null if not intersect, otherwise returns a new tuple with the intersection of the two
 	 */
 	public Tuple intersects(Tuple t) {
-		if (this.max > t.min)
-			return new Tuple(this.min, t.max);
-		return null;
+
+
+		if(!(t.getMax() <= this.min || t.getMin() >= this.max)) {
+			
+			if(t.getMin() > this.min) {
+				if(t.getMax() < this.max) {
+					// case 1
+					return new Tuple(t.getMin(),t.getMax());
+				}
+				else {
+					// case 2
+					return new Tuple(t.getMin(),this.max);
+				}
+			}
+			else {
+				if(t.getMax() < this.max) {
+					// case 3
+					return new Tuple(this.min ,t.getMax());
+				}
+				else{
+					// case 4
+					return new Tuple(this.min ,this.max);
+				}
+			}
+		}
+
+		else {
+			// case 5 && 6
+			return null;
+
+		}
 	}
-	
+
 	@Override
-    public int compareTo( final Tuple t) {
-        if (this.min > t.min) {
-        		return -1;
-        }else if (this.max < t.max) {
-        		return 1;
-        } else {
-        		return 0;
-        }
-    }
-	
+	public int compareTo( final Tuple t) {
+		if (this.min > t.min) {
+			return -1;
+		}else if (this.max < t.max) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
 	public String toString() {
 		return "(" + this.min + ", " + this.max + ")";
 	}
-		
+
 
 } 
--- src/main.java
+++ src/main.java
@@ -3,13 +3,11 @@ public class main {
 
 	public static void main(String[] args) {
 
-	Tuple subset1 = new Tuple(-23.00, 0.00);
-	Tuple subset2 = new Tuple(1.0, 100.00);
-	ArrayList<Tuple> subsets = new ArrayList<>();
-	subsets.add(subset1);
-	subsets.add(subset2);
-	Interval MyInterval = new Interval(subsets);
-	System.out.println(MyInterval.toString());
+	Tuple subset1 = new Tuple(-23.00, 10.00);
+	Tuple subset2 = new Tuple(10.0, 230.00);
+	
+	Tuple intersection = subset1.intersects(subset2);
+	System.out.println(intersection);
 	
 	}
 
